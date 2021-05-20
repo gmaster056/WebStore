25b1a4763f7c9583afef1dbc044ac6d4e2f035a6
Alexandr A. Amelkin <gmaster056@gmail.com>
2021-05-21 01:48:50 +0300

    Added views with mock data

--- src/WebStore/Models/Employee.cs
+++ src/WebStore/Models/Employee.cs
@@ -3,15 +3,11 @@ namespace WebStore.Models
 {
     public class Employee
     {
-        public Guid Id { get; set; }
-
+        public int Id { get; set; }
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
-
         public string MiddleName { get; set; }
-
-        public int Age { get; set;
-
+        public int Age { get; set; }
+        public string Job { get; set; }
     }
 }
