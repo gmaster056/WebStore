317fc89db14dd67a5a372b489b20ab7cfbc5d31f
Alexandr A. Amelkin <gmaster056@gmail.com>
2021-05-20 21:39:03 +0300

    Added employee model

---
+++ src/WebStore/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace WebStore.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public int Age { get; set;
+
+    }
+}
