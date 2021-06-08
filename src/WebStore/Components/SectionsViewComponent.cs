701310c817f922e0710d929779c66e1ce65eca9d
Alexander A <gmaster056@gmail.com>
2021-06-09 00:27:59 +0300

    Created the Sections component

---
+++ src/WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        private IProduct _Products;
+
+        public SectionsViewComponent(IProduct Products) => _Products = Products;
+
+        public IViewComponentResult Invoke()
+        {
+            var sections = _Products.GetSections();
+
+            var parentSections = sections.Where(s => s.ParentId == null);
+
+            var parentSectionsViews = parentSections
+               .Select(s => new SectionViewModel
+               {
+                   Id = s.Id,
+                   Name = s.Name,
+                   Order = s.Order,
+               })
+               .ToList();
+
+            foreach (var parentSection in parentSectionsViews)
+            {
+                var childs = sections.Where(s => s.ParentId == parentSection.Id);
+
+                foreach (var childSection in childs)
+                    parentSection.ChildSections.Add(new SectionViewModel
+                    {
+                        Id = childSection.Id,
+                        Name = childSection.Name,
+                        Order = childSection.Order,
+                        Parent = parentSection,
+                    });
+
+                parentSection.ChildSections.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+            }
+
+            parentSectionsViews.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+
+            return View(parentSectionsViews);
+        }
+}
+    }
--- src/WebStore/Controllers/ShopController.cs
+++ src/WebStore/Controllers/ShopController.cs
@@ -1,14 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     [Route("")]
     public class ShopController : Controller
     {
+        private readonly IProduct _Product;
+
+        public ShopController(IProduct Product) => _Product = Product;
+
         [Route("")]
-        public IActionResult Index()
+        public IActionResult Index(int? BrandId, int? SectionId)
         {
-            return View();
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+
+            var products = _Product.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+                Products = products
+                   .OrderBy(p => p.Order)
+                   .Select(p => new ProductViewModel
+                   {
+                       Id = p.Id,
+                       Name = p.Name,
+                       Price = p.Price,
+                       ImageUrl = p.ImageUrl,
+                   })
+            });
         }
     }
 }
